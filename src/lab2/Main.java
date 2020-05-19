c866cb23f2a6ea71a0f2b537fbdec48fb521b656
Whiteblodd <fridman999@gmail.com>
2020-05-19 18:02:12 +0300

    лаба 5

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -2,11 +2,11 @@ package lab2;
 
 public class Main {
     public static void main(String[] args) {
-        Point3d point1 = new Point3d(1,1,1); // Создание двух точек
-        Point3d point_zero = new Point3d();
+        Point3d point1 = new Point3d(2,4,6); // Создание двух точек
+        Point3d point2 = new Point3d(44, 3,2);
 
-        boolean is_same = Point3d.isSamePoints(point1,point_zero);// Сравнение координат
-        double distance = Point3d.distanceTo(point1, point_zero);// Вычисление расстояния между точками
+        boolean is_same = Point3d.isSamePoints(point1,point2);// Сравнение координат
+        double distance = Point3d.distanceTo(point1, point2);// Вычисление расстояния между точками
         System.out.println(is_same);
 
 
